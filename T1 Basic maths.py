8e2a2b48624f61562de680127809db03f058587e
sucheta07 <86112442+sucheta07@users.noreply.github.com>
2025-04-07 10:44:46 +0530

    Add files via upload

---
+++ T1 Basic maths.py
@@ -0,0 +1,11 @@
+# Basic Maths operation
+
+#Inputs 
+a = int(input("Entry the first Number : "))
+b = int(input("Entry the second Number : "))
+
+#outputs
+print("Addition : ", a+b)
+print("Substraction : ", a-b)
+print("Multiplication : ", a*b)
+print("Division : ", a/b)
---
+++ T2 Personised Message.py
@@ -0,0 +1,6 @@
+#taking user input
+a = input("Enter your First name : ")
+b = input("Enter your Last name : ")
+
+#printing the message output
+print(f"Hello,",a,b, "Welcome to the Python program.")
